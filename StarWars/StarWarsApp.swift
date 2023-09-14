f5d7da3acc802ec46bb97f6cb976e91191cfdd3d
Sahan Nagodavithana <sahan.nagodavithana@gmail.com>
2023-09-14 13:25:16 +0530

    Initial Commit

---
+++ StarWars/StarWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsApp.swift
+//  StarWars
+//
+//  Created by Sahan Nagodavithana on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
